4106bee4c99c9f5fc4c58888edfa327c9387231d
Derek <zengzhuoxi@gmail.com>
2016-11-26 17:35:01 -0500

    Added basic functionality. needs additional testing.

--- clippy.py
+++ clippy.py
@@ -7,8 +7,8 @@ import math
 import subprocess
 import random
 
-words_list =sys.argv[1:]
-length_of_input =  sum(len(x)+1 for x in words_list)
+message_list = sys.argv[1:]
+length_of_input =  sum(len(x) + 1 for x in message_list)
 max_length = 204  #70 - width of clippy and bubble * 4
 clippy = r"""
  ____
@@ -27,7 +27,35 @@ end_box = r"""
 |
 |
 |
+|
+|
 /
+"""
+
+angry_clippy = r"""
+                    _.-;:q=._
+                  .' j=""^k;:\.
+                 ; .F       ";`Y
+                ,;.J_        ;'j
+              ,-;"^7F       : .F           _________________
+             ,-'-_<.        ;gj. _.,---""''               .'
+            ;  _,._`\.     : `T"5,                       ;
+            : `?8w7 `J  ,-'" -^q. `                     ;
+             \;._ _,=' ;   n58L Y.                     .'
+               F;";  .' k_ `^'  j'                     ;
+               J;:: ;     "y:-='                      ;
+                L;;==      |:;   jT\                  ;
+                L;:;J      J:L  7:;'       _         ;
+                I;|:.L     |:k J:.' ,  '       .     ;
+                |;J:.|     ;.I F.:      .           :
+               ;J;:L::     |.| |.J  , '   `    ;    ;
+             .' J:`J.`.    :.J |. L .    ;         ;
+            ;    L :k:`._ ,',j J; |  ` ,        ; ;
+          .'     I :`=.:."_".'  L J             `.'
+        .'       |.:  `"-=-'    |.J              ;
+    _.-'         `: :           ;:;           _ ;
+_.-'"             J: :         /.;'       ;    ;
+='_ k;.. _.;:Y' , .' "---..__Y;."-=';:=' , .' ""
 """
 
 first_box = r"""
@@ -35,7 +63,9 @@ first_box = r"""
      /
      |
      |
+     |
   <--|
+     |
      |
      \_
 """
@@ -43,13 +73,58 @@ first_box = r"""
 
 def process_Command(command):
     command_List = command.split()
-    result = subprocess.call(command_List, shell = True)
+    result = subprocess.call(command_List[1:], shell = True)
     return result
 
 def print_Clippy(message):
     clippy = clippy.split("\n")
     first_box = first_box.split("\n")
     end_box = end_box.split("\n")
+    length_of_line = 0
+    words_per_line = 30
+    message_list = message.split()
+    #so there is enough space
+    #print words_per_line, length_of_line
+    usedwords = 0
+    for line in xrange(len(first_box)):
+        if ((line != len(first_box)-2 and line >= 3) ):
+            tempstr = 0
+            current_words = 0
+            while ((usedwords < len(message_list) and current_words < words_per_line)):
+                this_word = message_list[usedwords]
+                #output.append((length_of_line - len(this_word) ) * " ")
+                current_words += 1
+                usedwords +=1
+                tempstr += (len(this_word)+1)
+                if tempstr > length_of_line:
+                    length_of_line = tempstr
+    for line in xrange(len(first_box)):
+        if (line == 2) or (line == len(first_box)-2):
+            spacer = "_"
+        else:
+            spacer = " "
+        #Prints spacing, to prevent the text from appearing at the top.
+        if (line < len(first_box)/3) or (line == len(first_box)-2):
+            s = clippy[line] + " " + first_box[line] + ((length_of_line * spacer) + end_box[line])
+            print s
+        #Prints words
+        elif line >= len(first_box)/3:
+            output = []
+            output.append(clippy[line] + " ")
+            output.append(first_box[line])
+
+            current_words = 0
+            tempstr = 0
+
+            while (current_words < words_per_line) and (len(message_list) > 0):
+                this_word = (message_list).pop(0)
+                output.append(this_word + " ")
+                #output.append((length_of_line - len(this_word) ) * " ")
+                current_words += 1
+                tempstr += len(this_word)+1
+            output.append((length_of_line - tempstr) * " ")
+            output.append(end_box[line])
+            print "".join(output)
 
 def generate_Text():
 
--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import subprocess
 def test():
     subprocess.call(sys.argv[1:], shell=True)
 
+list1 = [1,2,3,4,5]
+
 if __name__ == "__main__":
-	while 1:
-		test()
+	print(len(list1))
+	for x in xrange(len(list1)):
+		print(x)
