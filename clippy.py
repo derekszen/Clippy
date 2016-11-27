15fbd49b58da6e2d60482ac7a3ba0f2fefdb424a
Derek <zengzhuoxi@gmail.com>
2016-11-27 00:34:24 +0000

    Basic functionality done, Ctrl+z needs to be stopped.

--- clippy.py
+++ clippy.py
@@ -6,15 +6,16 @@ import sys
 import math
 import subprocess
 import random
+import signal
 
 message_list = sys.argv[1:]
 length_of_input =  sum(len(x) + 1 for x in message_list)
 max_length = 204  #70 - width of clippy and bubble * 4
 clippy = r"""
- ____
+ ____       
 /    \
 |    |
-0  0
+0  0  
 ||  ||
 ||  ||
 |\_ /|
@@ -22,8 +23,8 @@ clippy = r"""
 """
 
 end_box = r"""
+
 \
-|
 |
 |
 |
@@ -40,28 +41,28 @@ angry_clippy = r"""
               ,-;"^7F       : .F           _________________
              ,-'-_<.        ;gj. _.,---""''               .'
             ;  _,._`\.     : `T"5,                       ;
-            : `?8w7 `J  ,-'" -^q. `                     ;
-             \;._ _,=' ;   n58L Y.                     .'
-               F;";  .' k_ `^'  j'                     ;
+            : `?8w7 `J  ,-'" -^q. `                     ; Y O U 
+             \;._ _,=' ;   n58L Y.                     .' 
+               F;";  .' k_ `^'  j'                     ;  H A V E 
                J;:: ;     "y:-='                      ;
-                L;;==      |:;   jT\                  ;
-                L;:;J      J:L  7:;'       _         ;
-                I;|:.L     |:k J:.' ,  '       .     ;
+                L;;==      |:;   jT\                  ;  B E E N 
+                L;:;J      J:L  7:;'       _         ;  
+                I;|:.L     |:k J:.' ,  '       .     ;  W A R N E D
                 |;J:.|     ;.I F.:      .           :
-               ;J;:L::     |.| |.J  , '   `    ;    ;
+               ;J;:L::     |.| |.J  , '   `    ;    ;    
              .' J:`J.`.    :.J |. L .    ;         ;
-            ;    L :k:`._ ,',j J; |  ` ,        ; ;
-          .'     I :`=.:."_".'  L J             `.'
+            ;    L :k:`._ ,',j J; |  ` ,        ; ;  
+          .'     I :`=.:."_".'  L J             `.'  DO NOT TRY TO EXIT AGAIN
         .'       |.:  `"-=-'    |.J              ;
     _.-'         `: :           ;:;           _ ;
 _.-'"             J: :         /.;'       ;    ;
 ='_ k;.. _.;:Y' , .' "---..__Y;."-=';:=' , .' ""
+
 """
 
 first_box = r"""
-      _
+
      /
-     |
      |
      |
   <--|
@@ -73,21 +74,27 @@ first_box = r"""
 
 def process_Command(command):
     command_List = command.split()
-    result = subprocess.call(command_List[1:], shell = True)
+    print(command_List)
+    result = subprocess.call(command_List, shell = True)
+    print(result)
     return result
 
 def print_Clippy(message):
-    clippy = clippy.split("\n")
-    first_box = first_box.split("\n")
-    end_box = end_box.split("\n")
+    global clippy
+    global first_box
+    global end_box
+
+    clippyList = clippy.split("\n")
+    first_boxList = first_box.split("\n")
+    end_boxList = end_box.split("\n")
     length_of_line = 0
     words_per_line = 30
     message_list = message.split()
     #so there is enough space
     #print words_per_line, length_of_line
     usedwords = 0
-    for line in xrange(len(first_box)):
-        if ((line != len(first_box)-2 and line >= 3) ):
+    for line in xrange(len(first_boxList)):
+        if ((line != len(first_boxList)-2 and line >= 3) ):
             tempstr = 0
             current_words = 0
             while ((usedwords < len(message_list) and current_words < words_per_line)):
@@ -98,20 +105,20 @@ def print_Clippy(message):
                 tempstr += (len(this_word)+1)
                 if tempstr > length_of_line:
                     length_of_line = tempstr
-    for line in xrange(len(first_box)):
-        if (line == 2) or (line == len(first_box)-2):
+    for line in xrange(len(first_boxList)):
+        if (line == 1) or (line == len(first_boxList)-2):
             spacer = "_"
         else:
             spacer = " "
         #Prints spacing, to prevent the text from appearing at the top.
-        if (line < len(first_box)/3) or (line == len(first_box)-2):
-            s = clippy[line] + " " + first_box[line] + ((length_of_line * spacer) + end_box[line])
+        if (line < len(first_boxList)/3) or (line == len(first_boxList)-2):
+            s = clippyList[line] + " " + first_boxList[line] + ((length_of_line * spacer) + end_boxList[line])
             print s
         #Prints words
-        elif line >= len(first_box)/3:
+        elif line >= len(first_boxList)/3:
             output = []
-            output.append(clippy[line] + " ")
-            output.append(first_box[line])
+            output.append(clippyList[line] + " ")
+            output.append(first_boxList[line])
 
             current_words = 0
             tempstr = 0
@@ -123,28 +130,45 @@ def print_Clippy(message):
                 current_words += 1
                 tempstr += len(this_word)+1
             output.append((length_of_line - tempstr) * " ")
-            output.append(end_box[line])
+            output.append(end_boxList[line])
             print "".join(output)
 
-def generate_Text():
-
+def generate_Text(command):
+    command_List = command.split()
     random.seed()
-    rand_Int = random.randint(0, sys.argv.len())
-    pregenerated_Text = ["Unable to find command.", sys.argv[rand_Int] + "is not a real word."]
-    rand_Int = random.randint(0, pregenerated_Text.len())
+    commandResult = "RESULT IS AN ERROR." 
+    rand_Int = random.randint(0, len(command_List)-1)
+    pregenerated_Text = ["Unable to find command.", command_List[rand_Int] + " is not a real word.", \
+                        "Would you like me to do this?", "This quite ineffective", \
+                        "You have mispelled " + command_List[rand_Int], \
+                        "Happily processing your command!", "Thank you, sir", \
+                        "The following are the result of your command:\n" + commandResult, \
+                        "ERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERROR"]
+    random.seed()
+    rand_Int = random.randint(0, len(pregenerated_Text)-1)
     text = pregenerated_Text[rand_Int]
-    return text    
+    return text
+
+def trigger_warning(signal, frame):
+    global angry_clippy
+    print(angry_clippy)
+    return 
+
+def TRIGGERED(signal, frame):
+    global nope
+    print(nope)
+    return 
 
 def main():
     #Welcome message 
     print_Clippy("Hello, my name is clippy, everyone's favorite personal assistant!")
     while 1:
-        command = raw_input("enter your next command:")
+        command = raw_input(">")
         process_Command(command)
-        message = generate_Text()
+        message = generate_Text(command)
         print_Clippy(message)
 
-
-
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, trigger_warning)
+    signal.signal(signal.SIGTSTP, TRIGGERED)
     main()
--- test.py
+++ test.py
@@ -4,11 +4,9 @@ import subprocess
 
 #Outputs the test
 def test():
-    subprocess.call(sys.argv[1:], shell=True)
+    result = subprocess.call(sys.argv[1:], shell=True)
+    print(result)
 
-list1 = [1,2,3,4,5]
 
 if __name__ == "__main__":
-	print(len(list1))
-	for x in xrange(len(list1)):
-		print(x)
+    test() 
